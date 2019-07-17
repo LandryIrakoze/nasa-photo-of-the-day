dce9363e00d4504573ced1df0e78567df37fe48a
Landry Irakoze <irakozel@gmail.com>
2019-07-17 18:50:53 -0400

    started dropdown

---
+++ src/DropDown.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+import Data from './Data';
+
+function DropDown() {
+
+// Grab and set state from Data.js
+
+
+}
+
+export default DropDown;
